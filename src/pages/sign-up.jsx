cae70f6609798e95000d38032184ace4332e9e17
Yash Verma <yash2229v@gmail.com>
2025-01-31 15:25:10 +0530

    3

--- src/pages/sign-up.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/clerk-react";
-
-export default function SignUpPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignUp routing="path" path="/sign-up" />
-    </div>
-  );
-}
