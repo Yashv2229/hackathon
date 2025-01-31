76c44c66e4f2cb416af7a479f556189b80725ae0
Yash Verma <yash2229v@gmail.com>
2025-01-31 14:26:10 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from "@clerk/clerk-react"
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = "pk_test_b3JnYW5pYy1odXNreS02NS5jbGVyay5hY2NvdW50cy5kZXYk"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key")
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
 )
